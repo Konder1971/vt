46ede2e819989c983189cb05e601eb6c5e50c9e6
Konder1971 <konder1971@mail.ru>
2020-12-15 13:28:32 +0200

    vt2

--- js/main.js
+++ js/main.js
@@ -18,3 +18,58 @@ const Counter = {
   template: ` Счётчик: {{ counter }} <button @click='sbros'>Сброс</button> `
 }
 Vue.createApp(Counter).mount('.counter')
+
+
+const Invvod = {
+  data() {
+    return {
+      vvod: ''
+    }
+  }
+}
+Vue.createApp(Invvod).mount('.invvod')
+
+const Ifseen = {
+  data() {
+    return {
+      seen: true
+    }
+  }
+}
+Vue.createApp(Ifseen).mount('.ifseen')
+
+const Olspisok = {
+  data() {
+    return {
+      spisok: [
+        {id: 1, text: 'lorem 1'},
+        {id: 2, text: 'lorem 2'},
+        {id: 3, text: 'lorem 3'},
+        {id: 4, text: 'lorem 4'}
+      ]
+    }
+  }
+}
+Vue.createApp(Olspisok).mount('.olspisok')
+
+
+const Comp = {
+  data() {
+    return {
+      spiskli: [
+        {id: 1, text: 'spiskli 1'},
+        {id: 2, text: 'spiskli 2'},
+        {id: 3, text: 'spiskli 3'},
+        {id: 4, text: 'spiskli 4'}
+      ]
+    }
+  }
+}
+const app = Vue.createApp(Comp)
+
+app.component('spisok-li', {
+  props: ['spis'],
+  template: `<li>{{ spis.text }} : {{ spis.id }}</li>`
+})
+
+app.mount('.comp')
