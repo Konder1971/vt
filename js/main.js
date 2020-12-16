2c883773da30a0de6c69bcf4837c1cd99868ed1e
Konder1971 <konder1971@mail.ru>
2020-12-16 11:43:30 +0200

    vt3

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@
 const Counter = {
   data() {
     return {
-      counter: 0
+      counter: 0,
+      colsbros: 0
     }
   },
   mounted() {
@@ -12,62 +13,43 @@ const Counter = {
   },
   methods: {
     sbros() {
-      this.counter = 0
+      this.counter = 0,
+      this.colsbros++
     }
   },
-  template: ` Счётчик: {{ counter }} <button @click='sbros'>Сброс</button> `
+  template: `Счётчик: {{ counter }} <button @click="sbros">Сброс</button> | количество сбросов: {{ colsbros }}`
 }
-Vue.createApp(Counter).mount('.counter')
+Vue.createApp(Counter).mount('#counter')
 
-
-const Invvod = {
+const Ullist = {
   data() {
     return {
-      vvod: ''
-    }
-  }
-}
-Vue.createApp(Invvod).mount('.invvod')
-
-const Ifseen = {
-  data() {
-    return {
-      seen: true
-    }
-  }
-}
-Vue.createApp(Ifseen).mount('.ifseen')
-
-const Olspisok = {
-  data() {
-    return {
-      spisok: [
-        {id: 1, text: 'lorem 1'},
-        {id: 2, text: 'lorem 2'},
-        {id: 3, text: 'lorem 3'},
-        {id: 4, text: 'lorem 4'}
+      lists: [
+        { id: 1, text: 'Lorem 1' },
+        { id: 2, text: 'Lorem 2' },
+        { id: 3, text: 'Lorem 3' }
       ]
     }
   }
 }
-Vue.createApp(Olspisok).mount('.olspisok')
+Vue.createApp(Ullist).mount('#list')
 
-
-const Comp = {
+const TodoList = {
   data() {
     return {
-      spiskli: [
-        {id: 1, text: 'spiskli 1'},
-        {id: 2, text: 'spiskli 2'},
-        {id: 3, text: 'spiskli 3'},
-        {id: 4, text: 'spiskli 4'}
+      groceryList: [
+        { id: 0, text: 'text 111' },
+        { id: 1, text: 'text 222' },
+        { id: 2, text: 'text 333' },
+        { id: 2, text: 'text 444' },
+        { id: 2, text: 'text 555' }
       ]
     }
   }
 }
-const sp = Vue.createApp(Comp)
-sp.component('spisok-li', {
-  props: ['spis'],
-  template: `<li>{{ spis.text }} : {{ spis.id }}</li>`
+const app = Vue.createApp(TodoList)
+app.component('todo-item', {
+  props: ['todo'],
+  template: `<li>{{ todo.text }}</li>`
 })
-sp.mount('.comp')
+app.mount('#todo-list-app')
